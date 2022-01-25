a2e3007bc9faa0f77f469b104ba09c684c4f5618
bismutoso <shadoxw_@hotmail.com>
2022-01-24 21:03:59 -0300

    removing debug

--- diversos/Password.py
+++ diversos/Password.py
@@ -3,18 +3,15 @@ from random import randint, choices, choice, sample
 
 
 class Generator:
-    def __init__(self, msg="", debug=False):
+    def __init__(self, msg=""):
         self.__msg = msg
         self.__symbols = None
-        self.__debug = debug
 
     @staticmethod
     def guide(msg):
-
         start = randint(0, 4)
         jump = randint(2, len(msg) // 2)
         index = np.arange(len(msg))[start::jump].tolist()
-
         return {"Start": start, "Jump": jump, "Index": index}
 
     @staticmethod
@@ -33,24 +30,19 @@ class Generator:
                               "?", "=", ">", "<", "∆", "π", "∑", "μ", "λ", "β", "δ",
                               "φ", "ω"], 35)
                }
-
         return rng
 
     def phrase(self):
         self.__symbols = self.table()
         symbol_list = choice(list(self.__symbols))
-
         upper_index = self.guide(self.__msg)  # Uppercase
         symbol_index = self.guide(self.__msg)  # Special characters
-
         rng_upper = randint(9, 9945)
         rng_symbol = randint(9, 9945)
-
         upper_msg = ""
         new_msg = ""
 
         for pos in range(len(self.__msg)):
-
             if pos in upper_index["Index"]:
                 upper_msg += str(self.__msg)[pos].upper()
 
@@ -58,9 +50,9 @@ class Generator:
                 upper_msg += str(self.__msg)[pos]
 
         for pos in range(len(self.__msg)):
-
             if pos in symbol_index["Index"]:
                 new_msg += str(upper_msg)[pos] + "".join((choices(self.__symbols[symbol_list], k=randint(1, 3))))
+
             else:
                 new_msg += str(upper_msg)[pos]
 
@@ -68,34 +60,11 @@ class Generator:
         num_symbol = str(sum(symbol_index["Index"]) + rng_symbol)
         password = num_upper + new_msg + num_symbol
 
-        if self.__debug:
-            return f"""
-| >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O>
-|Frase:{self.__msg}
-|Senha:{password}
-|Tamanho:{len(password)}
-| >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> 
-
-|Tabela:{symbol_list}
-
-|Sequência de letras minusculas
-|Começa na posição:{upper_index["Start"]}
-|Pulos:{upper_index["Jump"]}
-|Todas posições:{upper_index["Index"]}
-|Numero aleatório:{rng_upper}
-
-|Sequência de símbolos
-|Começa na posição:{symbol_index["Start"]}
-|Pulos:{symbol_index["Jump"]}
-|Todas posições:{symbol_index["Index"]}
-|Numero aleatório:{rng_symbol}
-"""
-        else:
-            return f"""
+        return f"""
 >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O>
-Frase:{self.__msg}
+Frase:|| {self.__msg} ||
 Senha:|| {password} ||
-Tamanho:{len(password)}
+Tamanho:|| {len(password)} ||
 >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> >-|-|-O> 
 """
 
