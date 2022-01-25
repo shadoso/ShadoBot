cfdbb283e8fa5934bfe4dc806193da99162d95a6
bismutoso <shadoxw_@hotmail.com>
2022-01-25 01:29:33 -0300

    handling errors

---
+++ diversos/database.py
@@ -0,0 +1,43 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+PATH = "/home/bismutoso/PycharmProjects/ShadoBot/key/shadobot-key.json"
+
+
+class Manager:
+    def __init__(self, discord_id, name):
+        scopes = SCOPE
+        credentials = Credentials.from_service_account_file(PATH, scopes=scopes)
+        gc = gspread.authorize(credentials)
+
+        self.__open = gc.open("Shadobot").sheet1
+        self.__discord_id = discord_id
+        self.__name = name
+
+    def create_user(self):
+        user = [
+            self.__discord_id,
+            self.__name,
+            "Novo usuário",
+            "None",
+            "None",
+            "Criou uma conta",
+            "None",
+            "None",
+            "None",
+            "None",
+            "None",
+            9.45,
+        ]
+
+        self.__open.insert_row(user, 2)
+        return True
+
+    def verify_user(self):
+        local = self.__open.find(self.__discord_id)
+        return True if local is not None else False
+
+    def show_info(self):
+        info = self.__open.find(self.__discord_id)
+        return self.__open.row_values(info.row)
