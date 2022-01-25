9369d284fc635f2ec8c51b660a0ede9de76462a1
bismutoso <shadoxw_@hotmail.com>
2022-01-24 21:21:22 -0300

    adding create account

--- cogs/Profile.py
+++ cogs/Profile.py
@@ -6,6 +6,8 @@ from google.oauth2.service_account import Credentials
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 PATH = "/home/bismutoso/PycharmProjects/ShadoBot/key/shadobot-key.json"
 MSG_UNIVERSE = "O universo é pequeno demais para contemplar seu maior feito!"
+MSG_FOUND_USER = "Você já tem uma conta"
+MSG_NEW_USER = "Conta criada no Shadosoverso"
 MSG_NO_USER = "Campo invalido ou usuário não registrado"
 
 DEEDS1 = 5
@@ -79,6 +81,20 @@ class Profile(commands.Cog):
 
         else:
             return await ctx.send(MSG_NO_USER)
+
+    @commands.command()
+    async def criar(self, ctx):
+        dcd_user_id = str(ctx.author.id)
+        dcd_user_name = str(ctx.author.name)
+        account = Manager(dcd_user_id, dcd_user_name)
+        verify = account.verify_user()
+
+        if verify:
+            return await ctx.send(MSG_FOUND_USER)
+
+        else:
+            account.create_user()
+            return await ctx.send(MSG_NEW_USER)
 
 
 def setup(bot):
--- cogs/TextCommands.py
+++ cogs/TextCommands.py
@@ -6,6 +6,8 @@ ping: Retorna o ping do bot
 fps: Quantos fps são necessários para rodar liso
 epic: Mostra os jogos grais da epic games
 senha: Cria uma senha com a palavra digitada (Experimental ainda, use so uma palavra e tudo em minusculo)
+criar: Cria seu perfil no Shadosoverso
+perfil: Mostra seu perfil no Shadosoverso
         """
 MSG_BOT_ERROR = "O bot ta pegando fogo bixo!"
 MSG_FPS = "**11 FPS ta lizinho!!!**"
