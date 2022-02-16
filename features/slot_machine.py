cc0b9488b3cfc68fb44aa2624363730dd33f90df
bismutoso <shadoxw_@hotmail.com>
2022-02-16 14:56:15 -0300

    Now all keys are achievable

--- features/slot_machine.py
+++ features/slot_machine.py
@@ -1,22 +1,32 @@
 from numpy.random import choice
-from numpy import array_equal
+from statistics import mode
 
-SLOT = [":x:", ":cherries:", ":blueberries:", ":coin:", ":credit_card:", ":gem:", ":8ball:"]
-TRIPLE = 3
-DOUBLE = 2
-JACKPOT = {":x:" * TRIPLE: 0,
-           ":cherries:" * TRIPLE: 50,
-           ":cherries:" * DOUBLE: 7,
-           ":blueberries:" * TRIPLE: 150,
-           ":blueberries:" * DOUBLE: 22,
-           ":coin:" * TRIPLE: 500,
-           ":coin:" * DOUBLE: 75,
-           ":credit_card:" * TRIPLE: 1500,
-           ":gem:" * TRIPLE: 2500,
-           ":8ball:" * TRIPLE: 25000
+KEY = 0
+VALUE = 1
+TWICE = 2
+THRICE = 3
+PROBABILITY = [0.30, 0.265, 0.179, 0.129, 0.073, 0.035, 0.019]
+NOTHING = ":x:"
+CHERRY = ":cherries:"
+BLUEBERRY = ":blueberries:"
+COIN = ":coin:"
+CARD = ":credit_card:"
+GEM = ":gem:"
+EIGHTBALL = ":8ball:"
+
+SLOT = [NOTHING, CHERRY, BLUEBERRY, COIN, CARD, GEM, EIGHTBALL]
+JACKPOT = {NOTHING * THRICE: 0,
+           CHERRY * THRICE: 50,
+           CHERRY * TWICE: 0.2,
+           BLUEBERRY * THRICE: 150,
+           BLUEBERRY * TWICE: 0.4,
+           COIN * THRICE: 500,
+           COIN * TWICE: 1.5,
+           CARD * THRICE: 1500,
+           GEM * THRICE: 2500,
+           EIGHTBALL * THRICE: 25000
            }
 
-PROBABILITY = [0.30, 0.265, 0.179, 0.129, 0.073, 0.035, 0.019]
 average = []
 
 
@@ -26,25 +36,28 @@ class SlotMachine:
 
     @staticmethod
     def spin():
-        row = choice(SLOT, size=TRIPLE, p=PROBABILITY)
-        key = "".join(row)
-        return key
+        row = choice(SLOT, size=THRICE, p=PROBABILITY)
+        key = "".join(row[:])
+        if key.count(CHERRY) == TWICE or key.count(BLUEBERRY) == TWICE or key.count(COIN) == TWICE:
+            key = mode(row) * TWICE
+        return key, "".join(row)
 
     def pot(self):
         result = self.spin()
-        print(result)
-        if result in JACKPOT:
-            return JACKPOT[result]
-
+        key = result[KEY]
+        value = result[VALUE]
+        if key in JACKPOT:
+            return JACKPOT[key], value
         else:
-            return None
+            return value
 
 
 if __name__ == '__main__':
     jacks = SlotMachine(100)
-    while len(average) < 2:
+    while len(average) < 15:
         guess = jacks.pot()
-        if guess is not None:
+        print(guess)
+        if type(guess) is not str:
             average.append(guess)
 
     print(average)
