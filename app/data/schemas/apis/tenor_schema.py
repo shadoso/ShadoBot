23f5ea5e122245c8aa7689cc1f8964ae770e264b
Gregory <gregoryspoladore@hotmail.com>
2022-11-22 20:22:50 -0300

    new tenor schema

--- app/data/schemas/apis/tenor_schema.py
+++ app/data/schemas/apis/tenor_schema.py
@@ -3,16 +3,24 @@ from typing import List
 
 
 class Url(BaseModel):
-    itemurl: str
+    url: str
+
+
+class Link(BaseModel):
+    mediumgif: Url
+
+
+class Formats(BaseModel):
+    media_formats: Link
 
 
 class Gif(BaseModel):
-    results: List[Url]
+    results: List[Formats]
 
     def get_url(self):
         url = []
 
         for gif in self.results:
-            url.append(gif.itemurl)
+            url.append(gif.media_formats.mediumgif.url)
 
         return url
