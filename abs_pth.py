80225fe61a9bd125648a6b21ea35c28b0801decc
Gregory <gregoryspoladore@hotmail.com>
2022-11-22 20:18:56 -0300

    abs_pth.py have new func names, and json_text returns dotted dict

--- abs_pth.py
+++ abs_pth.py
@@ -1,45 +1,51 @@
 import os
 import json
 
-FIRST_ELEMENT = 0
+
+class Dotted(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 
-def arquive(where: list):
+def root_path(where: list):
     """
     It only works if this file is in the root directory
     :param where: A list of directories
     :return: Absolute Path
     """
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    where.insert(FIRST_ELEMENT, root_path)
+    first_element = 0
+    abs_path = os.path.dirname(os.path.abspath(__file__))
+    where.insert(first_element, abs_path)
 
     return "/".join(where)
 
 
 def filenames(where: list):
-    return os.listdir(arquive(where=where))
+    return os.listdir(root_path(where=where))
 
 
-def json_file(where: list, commands: list):
-    command = []
+def json_text(where: list, commands: list):
+    all_cmd = {}
 
-    with open(file=arquive(where=where)) as file:
-        dic = json.load(file)
+    with open(file=root_path(where=where)) as file:
+        file = json.load(file)
 
     for item in commands:
-        command.append(dic[item])
+        all_cmd[item] = file[item]
 
-    return command
+    all_cmd = Dotted(all_cmd)
+    return all_cmd
 
 
 if __name__ == "__main__":
-    url = ["main", ".env"]
-    testing = arquive(url)
+    url = ["config", ".env"]
+    testing = root_path(url)
     print(testing)
     many_files = ["app", "old_not_in_use", "features"]
     testing1 = filenames(many_files)
     print(testing1)
     js = ["app", "languages", "info.json"]
     rs = ["language"]
-    testing2 = json_file(where=js, commands=rs)[0]
-    print(testing2["names"])
+    testing2 = json_text(where=js, commands=rs)
+    print(testing2.language)
