140948daab19a13aa2aee3f549836c40161df105
Grégory (Shadoso) <gregoryspoladore@hotmail.com>
2023-10-14 14:57:11 -0300

    Cleaning the code and adding FOREBEARS_API.

--- config/config.py
+++ config/config.py
@@ -1,4 +1,7 @@
-from pydantic import BaseSettings
+# Third-party
+from pydantic_settings import BaseSettings
+
+# Local
 from absolute_path import list_to_path
 
 
@@ -6,15 +9,18 @@ class Settings(BaseSettings):
     DATABASE_HOSTNAME: str
     DATABASE_NAME: str
     DATABASE_PASSWORD: str
-    DATABASE_PORT: str
+    DATABASE_PORT: int
     DATABASE_USERNAME: str
     DISCORD_ID: str
     DISCORD_KEY: str
     DISCORD_URL: str
-    TENOR_API: str
+    FOREBEARS_API: str
+    HASH_ALGORITHM: str
+    SECRET_KEY: str
     TELEGRAM_API: str
-    TELEGRAM_CHAT_ID: str
-    CKEY: str
+    TELEGRAM_CHAT_ID: int
+    TENOR_API: str
+    TENOR_CKEY: str
 
     class Config:
         env_file = list_to_path(file_path=["config", ".env"])
@@ -23,4 +29,4 @@ class Settings(BaseSettings):
 settings = Settings()
 
 if __name__ == "__main__":
-    print(settings.CKEY)
+    print(settings.DISCORD_URL)
