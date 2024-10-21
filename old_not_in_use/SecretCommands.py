310e4e7c7b2711ba23f6e47701781f800de2bd35
Grégory <gregoryspoladore@hotmail.com>
2024-10-21 16:02:23 -0300

    Deleted

--- old_not_in_use/SecretCommands.py
+++
@@ -1,8 +0,0 @@
-# web = discord.Embed(title="ERROR_404_NOT_FOUND", description=HACKER_DESCRIPTION, color=0x1d2b53)
-# web.set_thumbnail(url=HACKER)
-# web.add_field(name=":unlock: CHANGE_ORG_USER :coin: 99.945,99", value=ORG, inline=False)
-# await ctx.send(embed=web)
-
-# HACKER = "https://cdn.discordapp.com/attachments/935364491804303392/935422018311036958/image3A31064_eightbit.png"
-# HACKER_DESCRIPTION = "ERROR_404_NOT_FOUND " * 9
-# ORG = f"> Hacks the server and decodes the security hash-256 key"
